c6c73bef4bc641dfab83e6c86c27052fa26a0abe
Ken <imotok@g-hopper.ne.jp>
2017-07-05 17:02:52 +0900

    first commit

---
+++ sample_program.js
@@ -0,0 +1,15 @@
+//http://127.0.0.1:1234/
+
+var http = require('http');
+ 
+var server = http.createServer();
+server.on('request', doRequest);
+server.listen(process.env.PORT, process.env.IP);
+console.log('Server running!');
+ 
+// リクエストの処理
+function doRequest(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World\n');
+    res.end();
+}
